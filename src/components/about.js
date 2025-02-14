920368df5763b5581611973175e5d71747f3a7aa
Ahmed-Abdelgawad-Dev <ahmed.abdelgawad.dev@gmail.com>
2025-02-14 18:00:12 +0200

    initial code

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,8 @@ function About() {
             {/* <h2 className="justify-center items-center text-center">Contacts</h2><hr /> */}
             <h5>Mobile: +20 100 4059826</h5><hr />
             <h5>Email: gm@ilmercatohotel.com</h5><hr />
-        </div>
+            <a id="linkedin" href="https://www.linkedin.com/in/mohamed-abdel-gelil-93b49333/">LinkedIn</a>
+        </div >
     )
 }
 
